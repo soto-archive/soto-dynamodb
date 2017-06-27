baecad58fd169ac6c0f78c2221434da2ebdaef9e
noppoman <yuki@miketokyo.com>
2017-06-27 13:59:03 +0900

    copy from aws-sdk-swift

---
+++ Package.swift
@@ -0,0 +1,11 @@
+// swift-tools-version:3.1
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftAWSDynamodb",
+    dependencies: [
+        .Package(url: "https://github.com/noppoMan/aws-sdk-swift-core.git", majorVersion: 0, minor: 1)
+    ]
+)
+  

dbd14bfb6ee438975c796c9d8733420e8131599c
Adam Fowler <adamfowler71@gmail.com>
2020-06-02 16:53:11 +0100

    Version 4.7.0

--- Package.swift
+++ Package.swift
@@ -1,16 +1,16 @@
 // swift-tools-version:5.0
-
 import PackageDescription
 
 let package = Package(
-  name: "DynamoDB",
-  products: [
-      .library(name: "DynamoDB", targets: ["DynamoDB"]),
-  ],
-  dependencies: [
-      .package(url: "https://github.com/swift-aws/aws-sdk-swift-core.git", .upToNextMinor(from: "4.2.0"))
-  ],
-  targets: [
-      .target(name: "DynamoDB", dependencies: ["AWSSDKSwiftCore"]),
-  ]
+    name: "DynamoDB",
+    platforms: [.iOS(.v12), .tvOS(.v12), .watchOS(.v5)],
+    products: [
+        .library(name: "DynamoDB", targets: ["DynamoDB"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/swift-aws/aws-sdk-swift-core.git", .upToNextMinor(from: "4.6.0"))
+    ],
+    targets: [
+        .target(name: "DynamoDB", dependencies: ["AWSSDKSwiftCore"], path: "./Sources/DynamoDB"),
+    ]
 )

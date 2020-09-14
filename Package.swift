4bdf4eef1e902896ba15e532d74db8a370a9ef42
Adam Fowler <adamfowler71@gmail.com>
2020-09-14 10:55:55 +0100

    Sync with Soto v4.9.0

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .library(name: "DynamoDB", targets: ["DynamoDB"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-aws/aws-sdk-swift-core.git", .upToNextMinor(from: "4.6.0"))
+        .package(url: "https://github.com/soto-project/soto-core.git", .upToNextMinor(from: "4.7.0"))
     ],
     targets: [
         .target(name: "DynamoDB", dependencies: ["AWSSDKSwiftCore"], path: "./Sources/DynamoDB"),
